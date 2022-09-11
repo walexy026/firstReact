83856ec73bcd7c152e27fb275be1a734d83f3016
olawale <jao.olaniyi@gmail.com>
2022-09-11 14:27:12 +0100

    added react icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
 import Header from "./components/Header";
+import Tasks  from "./components/Tasks";
+import { useState } from "react"
 
 function App() {
+  const [ tasks, setTasks] = useState([  {
+    id : 1,
+    text: 'Doctors Assignment',
+    day: 'Feb 5th at 2:30pm',
+    reminder: true
+},
+{
+    id : 2,
+    text: 'Doctors Appointment',
+    day: 'Feb 5th at 2:30pm',
+    reminder: true
+
+},
+{
+    id : 3,
+    text: 'Meeting at the school',
+    day: 'Feb 5th at 2:30pm',
+    reminder: false
+
+},
+{
+    id : 4,
+    text: 'Food shooping',
+    day: 'Feb 5th at 2:30pm',
+    reminder: true
+
+},
+{
+    id : 5,
+    text: 'Clubbing',
+    day: 'Feb 5th at 2:30pm',
+    reminder: false
+
+}
+])
+// deleteTask
+const deleteTask =(id) =>{
+  setTasks(tasks.filter((task)=>task.id !== id))
+}
   return (
     <div className="container">
     <Header/>
+   {tasks.length > 0 ?( <Tasks tasks ={tasks} onDelete ={deleteTask}/>) :(
+    'No tasks to Show'
+   )}
     </div>
     
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,29 @@
-const Header = () => {
-  return <h1 style={headingStyle}>header</h1>;
+import PropTypes from 'prop-types'
+import Button from "./Button";
+
+const Header = ({title}) => {
+    const onClick = () => {
+        console.log('Click')
+    }
+  return (
+  <header className="header">
+      <h1>{title}</h1>
+      <Button color = "green" text ="Add" onclick={onClick}/>
+     
+  </header>  
+  )
+  
 };
-const headingStyle = {
-  color: "red",
-  backgroundColor: "black",
-  textAlign: "center",
-};
+Header.defaultProps ={
+    title:'Task Tracker',
+}
+Header.propTypes = {
+    title:PropTypes.string.isRequired,
+}
+// const headingStyle = {
+//   color: "red",
+//   backgroundColor: "black",
+//   textAlign: "center",
+// };
 
 export default Header;
