2e51dc8f5579d0469d777511f9724ed1bb7c7178
olawale <jao.olaniyi@gmail.com>
2022-09-11 09:49:39 +0100

    styling of the task tracker

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return <h1 style={headingStyle}>header</h1>;
+};
+const headingStyle = {
+  color: "red",
+  backgroundColor: "black",
+  textAlign: "center",
+};
+
+export default Header;
